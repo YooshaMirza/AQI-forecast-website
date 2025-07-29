01fc045bedefd06ca64b2e74214d10fb9c3d4fb4
MIRZA YOOSHA MINHAJ <143473452+YooshaMirza@users.noreply.github.com>
2025-07-29 12:06:11 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,12 +2,9 @@
 import os
 import sys
 
-# Add the root directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-# Import and run the Flask app
-from main import app as application
+from main import app
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    application.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
